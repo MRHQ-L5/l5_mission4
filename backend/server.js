8ce797ff55a1e085b2c99c958c7c1f1372bd467c
MHS Technologies <mhstechcompany@gmail.com>
2022-05-11 23:41:15 +1200

    It delivers first proto that communicates with IBM Discovery

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const DiscoveryV1 = require('ibm-watson/discovery/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const discovery = new DiscoveryV1({
+  version: '3019-04-30',
+  authenticator: new IamAuthenticator({
+    apikey: 'kwkguJ9mHc2AUfYuP1hZ_FANBh0I4fgWQ9tt9LxpEXpB',
+  }),
+  serviceUrl: 'https://api.eu-gb.discovery.watson.cloud.ibm.com/instances/fa3b274f-ec83-4a61-84fd-6959e49d5bda',
+});
 
 dotenv.config();
 
@@ -24,8 +34,19 @@ server.listen(port, () => {
 server.post('/submit_query', (req, res) => {
   const { pack } = req.body;
   
-  console.log(pack);
-  //Use IBM Search Engine
-  
-  res.send('good');
+  const queryParams = {
+    environmentId: 'dfc85c58-5f1f-4b7c-b4f4-f5ecdc9ce629',
+    collectionId: '63ed94bb-bc7d-487b-a3e3-13981a6d19b6',
+    query: pack,
+  };
+
+  discovery.query(queryParams)
+    .then(queryResponse => {
+      console.log(JSON.stringify(queryResponse, null, 2));
+      res.send(JSON.stringify(queryResponse, null, 2));
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
 });
+
